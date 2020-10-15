84973e17651866cf339cb2373fa2185b4db69cfb
tsioutsn <61215311+tsioutsn@users.noreply.github.com>
2020-10-15 15:53:01 -0500

    Completed Activity 4x2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
+class App extends React.Component {
+ state = {
+   data: null
+ }
+
+ componentDidMount() {
+   Axios.get('http://localhost:5000')
+   .then((response) => {
+     this.setState({
+       data: response.data
+     })
+   })
+   .catch((error) => {
+     console.error('Error fetching data: ${error}');
+   })
+ }
+ 
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          GoodThings!
       </header>
     </div>
   );
+  }
 }
 
 export default App;
