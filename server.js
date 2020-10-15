23b9bf7358d835f9d3272c97e261760bd2aa51c6
tsioutsn <61215311+tsioutsn@users.noreply.github.com>
2020-10-15 15:00:18 -0500

    updated server.js

--- server.js
+++ server.js
@@ -5,8 +5,26 @@ const app = express();
 
 connectDatabase();
 
+app.use(express.json({ extended: false }));
+
+// API endpoints
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
+
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+/**
+ * @route POST api/users
+ * @desc Register userconsole.log(req.body);
+ */
+
+ app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+ });
 
 app.listen(3000, () => console.log('Express server running on port 3000'));
