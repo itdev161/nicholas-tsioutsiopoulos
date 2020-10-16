60d439b1d390f70ebf3eb111f7e7a3736700070c
tsioutsn <61215311+tsioutsn@users.noreply.github.com>
2020-10-15 21:40:24 -0500

    JWT token

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 import User from './models/User';
 
 const app = express();
@@ -69,7 +71,23 @@ app.post(
 
                 //Save the db and return
                 await user.save();
-                res.send('User successfully registered');
+             
+                // Generate and return a JWT token
+                const payload = {
+                    user: {
+                        id: user.id
+                    }
+                };
+
+                jwt.sign(
+                    payload,
+                    config.get('jwtSecret'),
+                    { expiresIn: '10hr' },
+                    (err, token) => {
+                        if (err) throw err;
+                        res.json({ token: token });
+                    }
+                );
             } catch (error) {
                 res.status(500).send('Server error');
             }
