677c53e26021263c2b80f9cb83227bb6081cc25b
tsioutsn <61215311+tsioutsn@users.noreply.github.com>
2020-10-28 18:33:52 -0500

    Added Links and Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Axios from 'axios';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 class App extends React.Component {
  state = {
@@ -22,11 +23,37 @@ class App extends React.Component {
  
   render() {
     return (
+      <Router>
     <div className="App">
       <header className="App-header">
-          GoodThings!
+          <h1>GoodThings</h1>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
       </header>
+      <main>
+        <Route exact path="/">
+          {this.state.data}
+        </Route>
+        <Switch>
+          <Route path="/register">
+            Register
+          </Route>
+          <Route path="/login">
+            Login
+          </Route>
+        </Switch>
+      </main>
     </div>
+    </Router>
   );
   }
 }
