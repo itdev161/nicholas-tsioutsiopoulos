ce223fd150c2c2b9b8480182d5f5223226e13b42
tsioutsn <61215311+tsioutsn@users.noreply.github.com>
2020-11-11 21:02:40 -0600

    Updated Server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import config from 'config';
 import User from './models/User';
+import Post from './models/Post';
 import auth from './middleware/auth';
 
 const app = express();
@@ -157,6 +158,53 @@ app.post(
          );
      };
 
+     // Post endpoints
+     /**
+      * @route POST api/posts
+      * @desc Create post
+      */
+
+      app.post(
+          '/api/posts',
+          [
+              auth,
+              [
+                check('title', 'Title text is required')
+                  .not()
+                  .isEmpty(),
+                check('body', 'Body text is required')
+                .not()
+                .isEmpty()
+              ]
+          ],
+          async (req, res) => {
+              const errors = validationResult(req);
+              if (!errors.isEmpty()) {
+                  res.status(400).json({ errors: errors.array() });
+              } else {
+                  const { title, body } = req.body;
+                  try {
+                      // Get the user who created the post
+                      const user = await User.findById(req.user.id);
+
+                      // Create a new post
+                      const post = new Post({
+                          user: user.id,
+                          title: title,
+                          body: body
+                      });
+
+                      // Save to the db and return
+                      await post.save();
+
+                      res.json(post);
+                  } catch (error) {
+                      console.error(error);
+                      res.status(500).send('Server error');
+                  }
+              }
+          }
+      );
 
 const port = 5000;
 app.listen(port, () => console.log('Express server running on port 5000'));
